6e9bfe850a412a4a6136cc3ebe8a0b650fc478cc
Mateusz Detko <mateuszdetko@MacBook-Air-Mateusz.local>
2016-11-08 16:55:33 +0100

    klasa Rectangle

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 class Vector
 {
-private:
+    private:
     int firstCord;
     int secondCord;
     
-public:
+    public:
     Vector(int x, int y) {
         this->firstCord = x;
         this->secondCord = y;
@@ -25,7 +25,7 @@ public:
     }
     
     Vector reflection() {
-        return Vector(secondCord, firstCord);
+        return Vector(secondCord, firstCord); // czy na pewno dobrze ??
     }
     
     int x(void) {
@@ -39,14 +39,87 @@ public:
 
 class Rectangle
 {
+    private:
+    int rWidth;
+    int rHeight;
+    pair<int, int> rPos;
+    
+    public:
+    Rectangle(int width, int height) {
+        this->rWidth = width;
+        this->rHeight = height;
+        this->rPos = make_pair(0, 0);
+    }
+    
+    Rectangle(int width, int height, pair<int, int> pos) {
+        this->rWidth = width;
+        this->rHeight = height;
+        this->rPos = pos;
+    }
+    
+    bool operator == (const Rectangle& r) const {
+        return (this->rWidth == r.rWidth && this->rHeight == r.rHeight && this->rPos == r.rPos);
+    }
+    
+    Rectangle operator += (Vector& v) {
+        int leftDownX = get<0>(rPos);
+        int leftDownY = get<1>(rPos);
+        leftDownX += v.x();
+        leftDownY += v.y();
+        rPos = make_pair(leftDownX, leftDownY);
+        return *this; //czy na pewno * ??
+    }
+    
+    Rectangle reflection() {
+        int leftDownX = get<0>(rPos);
+        int leftDownY = get<1>(rPos);
+        int rightDownX = leftDownX + rWidth;
+        int rightUpY = leftDownY + rHeight;
+        return Rectangle(rWidth, rHeight, make_pair(-rightDownX, -rightUpY)); // czy dobrze ??
+    }
+    
+    int area() {
+        return rWidth*rHeight;
+    }
+    
+    pair<Rectangle, Rectangle> split_horizontally(int place) {
+        int leftDownX = get<0>(rPos);
+        int leftDownY = get<1>(rPos) + place;
+        pair<int, int> newPos = make_pair(leftDownX, leftDownY);
+        
+        return make_pair(Rectangle(rWidth, place, rPos), Rectangle(rWidth, rHeight-place, newPos));
+    }
+    
+    pair<Rectangle, Rectangle> split_vertically(int place) {
+        int leftDownX = get<0>(rPos) + place;
+        int leftDownY = get<1>(rPos);
+        pair<int, int> newPos = make_pair(leftDownX, leftDownY);
+        
+        return make_pair(Rectangle(place, rHeight, rPos), Rectangle(rWidth-place, rHeight, newPos));
+    }
+    
+    int width() {
+        return rWidth;
+    }
+    
+    int height() {
+        return rHeight;
+    }
+    
+    pair<int, int> pos() {
+        return rPos;
+    }
     
 };
 
 int main(int argc, const char * argv[]) {
-    Vector wektor = Vector(100, 200);
-    Vector wektor2 = Vector(400, 100);
-    wektor += wektor2;
-    cout << wektor.x() << endl;
-    cout << wektor.y() << endl;
+    Rectangle rect1 = Rectangle(100, 200);
+    pair<Rectangle, Rectangle> para = rect1.split_vertically(20);
+    
+    Rectangle rect2 = get<0>(para);
+    Rectangle rect3 = get<1>(para);
+    
+    cout << get<0>(rect2.pos()) << "," << get<1>(rect2.pos()) << " h: " << rect2.height() << " w: "<< rect2.width() << endl;
+    cout << get<0>(rect3.pos()) << "," << get<1>(rect3.pos()) << " h: " << rect3.height() << " w: "<< rect3.width() << endl;
     return 0;
 }
