f901e6c4b66d9f19633751c4fd378b4d8802d61a
Day1721 <puzynamarek97@gmail.com>
2016-11-12 21:29:48 +0100

    Add CMakeLists.txt (default created) Split all declarations with implementations between *.h and *.cc Debug now project can be compiled

--- geometry.cc
+++ geometry.cc
@@ -1,222 +1,201 @@
 #include <iostream>
+#include <utility>
 #include <vector>
+#include "geometry.h"
 
 using namespace std;
 
-class Vector
-{
-    private:
-        int firstCord;
-        int secondCord;
+/*
+ - funkcja origin w klasie Position
+ - funkcja reflection w klasie Rectangle
+ - funkcje merge_horizontally i merge_vertically
+ - plik naglowkowy .h
+ - r-value
+ - sprawdzenie poprawnosci argumentow (assert?)
+*/
 
-    public:
-        Vector(int x, int y) {
-            this->firstCord = x;
-            this->secondCord = y;
-        }
-        
-        bool operator == (const Vector& v) const {
-            return (this->firstCord == v.firstCord && this->secondCord == v.secondCord);
-        }
-        
-        Vector& operator += (const Vector& v) {
-            this->firstCord += v.firstCord;
-            this->secondCord += v.secondCord;
-            return *this;
-        }
-        
-        Vector reflection() {
-            return Vector(secondCord, firstCord);
-        }
-        
-        int x(void) {
-            return firstCord;
-        }
-        
-        int y(void) {
-            return secondCord;
-        }
-};
+//---------------------------------- VECTOR ----------------------------------
 
-class Position
-{
-    private:
-        int firstCord;
-        int secondCord;
-    
-    public:
-        Position(int x, int y) {
-            this->firstCord = x;
-            this->secondCord = y;
-        }
-        
-        bool operator == (const Position& p) const {
-            return (this->firstCord == p.firstCord && this->secondCord == p.secondCord);
-        }
-        
-        Position& operator += (Vector& v) {
-            if (firstCord != 0 && secondCord != 0) {
-                this->firstCord += v.x();
-                this->secondCord += v.y();
-            }
-            return *this;
-        }
-        
-        Position reflection() {
-            return Position(secondCord, firstCord);
-        }
-        
-        int x(void) {
-            return firstCord;
-        }
-        
-        int y(void) {
-            return secondCord;
-        }
-        
-        static Position origin() {
-            return Position(0, 0); // czy dobrze ??
-        }
-};
+Vector::Vector(int x, int y) {
+    this->firstCord = x;
+    this->secondCord = y;
+}
 
-class Rectangle
-{
-    private:
-        int widthValue;
-        int heightValue;
-        Position position;
-    
-    public:
-        Rectangle(int width, int height) : position(Position(0, 0)) {
-            this->widthValue = width;
-            this->heightValue = height;
-        }
-        
-        Rectangle(int width, int height, Position pos) : position(pos) {
-            this->widthValue = width;
-            this->heightValue = height;
-            this->position = pos;
-        }
-        
-        bool operator == (const Rectangle& r) const {
-            return (this->widthValue == r.widthValue && this->heightValue == r.heightValue && this->position == r.position);
-        }
-        
-        Rectangle& operator += (Vector& v) {
-            position += v;
-            return *this;
-        }
-        
-        Rectangle reflection() { // czy dobrze ??
-            int rightDownX = position.x() + widthValue;
-            int rightUpY = position.y() + heightValue;
-            return Rectangle(widthValue, heightValue, Position(-rightDownX, -rightUpY));
-        }
-        
-        long long area() {
-            return widthValue * heightValue;
-        }
-        
-        pair<Rectangle, Rectangle> split_horizontally(int place) {
-            int leftDownX = position.x();
-            int leftDownY = position.y() + place;
-            Position newPos = Position(leftDownX, leftDownY);
-            
-            return make_pair(Rectangle(widthValue, place, position), Rectangle(widthValue, heightValue-place, newPos));
-        }
-        
-        pair<Rectangle, Rectangle> split_vertically(int place) {
-            int leftDownX = position.x() + place;
-            int leftDownY = position.y();
-            Position newPos = Position(leftDownX, leftDownY);
-            
-            return make_pair(Rectangle(place, heightValue, position), Rectangle(widthValue-place, heightValue, newPos));
-        }
-        
-        int width() {
-            return widthValue;
-        }
-        
-        int height() {
-            return heightValue;
-        }
-        
-        Position pos() {
-            return position;
-        }
-};
+bool Vector::operator ==(const Vector& v) const {
+    return (this->firstCord == v.firstCord && this->secondCord == v.secondCord);
+}
 
-class Rectangles
-{
-    private:
-        vector<Rectangle> recsVector;
-    
-    public:
-        Rectangles() {}
+Vector& Vector::operator +=(const Vector& v) {
+    this->firstCord += v.firstCord;
+    this->secondCord += v.secondCord;
+    return *this;
+}
 
-        Rectangles(vector<Rectangle> recsVector) {
-            this->recsVector = recsVector;
-        }
+Vector Vector::reflection() {
+    return Vector(secondCord, firstCord);
+}
 
-        bool operator == (const Rectangles& recsObj) const {
-            return (this->recsVector == recsObj.recsVector);
-        }
+int Vector::x() const {
+    return firstCord;
+}
 
-        Rectangles& operator += (Vector& v) {
-            for (int i = 0; i < recsVector.size(); i++) {
-                recsVector[i] += v;
-            }
-            return *this;
-        }
+int Vector::y() const {
+    return secondCord;
+}
 
-        Rectangle& operator [] (int i) {
-            return recsVector[i];
-        }
-        
-        size_t size() {
-            return recsVector.size();
-        }
+//--------------------------------- Position ---------------------------------
 
-        void split_horizontally(int idx, int place) {
-            pair<Rectangle, Rectangle> result = recsVector[idx].split_horizontally(place);
-            recsVector.erase(recsVector.begin()+idx);
-            recsVector.insert(recsVector.begin()+idx, get<0>(result));
-            recsVector.insert(recsVector.begin()+idx+1, get<1>(result));
-        }
+Position::Position(int x, int y) {
+    this->firstCord = x;
+    this->secondCord = y;
+}
 
-        void split_vertically(int idx, int place) {
-            pair<Rectangle, Rectangle> result = recsVector[idx].split_vertically(place);
-            recsVector.erase(recsVector.begin()+idx);
-            recsVector.insert(recsVector.begin()+idx, get<0>(result));
-            recsVector.insert(recsVector.begin()+idx+1, get<1>(result));
-        }
-};
+bool Position::operator ==(const Position& p) const {
+    return (this->firstCord == p.firstCord && this->secondCord == p.secondCord);
+}
 
-Position operator + (Position p, Vector &v) {
+Position Position::reflection() {
+    return Position(secondCord, firstCord);
+}
+
+int Position::x() const {
+    return firstCord;
+}
+
+int Position::y() const {
+    return secondCord;
+}
+
+Position Position::origin() {
+    return Position(0, 0); // czy dobrze ?? TAK
+}
+
+//--------------------------------- Rectangle ---------------------------------
+
+Rectangle::Rectangle(int width, int height) : position(Position(0, 0)) {
+    this->widthValue = width;
+    this->heightValue = height;
+}
+
+Rectangle::Rectangle(int width, int height, Position pos) : position(pos) {
+    this->widthValue = width;
+    this->heightValue = height;
+    this->position = pos;
+}
+
+bool Rectangle::operator ==(const Rectangle& r) const {
+    return (this->widthValue == r.widthValue && this->heightValue == r.heightValue && this->position == r.position);
+}
+
+Rectangle& Rectangle::operator +=(Vector& v) {
+    position += v;
+    return *this;
+}
+
+Rectangle Rectangle::reflection() { // czy dobrze ??
+    int rightDownX = position.x() + widthValue;
+    int rightUpY = position.y() + heightValue;
+    return Rectangle(widthValue, heightValue, Position(-rightDownX, -rightUpY));
+}
+
+long long Rectangle::area() {
+    return widthValue * heightValue;
+}
+
+std::pair<Rectangle, Rectangle> Rectangle::split_horizontally(int place) {
+    int leftDownX = position.x();
+    int leftDownY = position.y() + place;
+    Position newPos = Position(leftDownX, leftDownY);
+
+    return std::make_pair(Rectangle(widthValue, place, position), Rectangle(widthValue, heightValue - place, newPos));
+}
+
+std::pair<Rectangle, Rectangle> Rectangle::split_vertically(int place) {
+    int leftDownX = position.x() + place;
+    int leftDownY = position.y();
+    Position newPos = Position(leftDownX, leftDownY);
+
+    return std::make_pair(Rectangle(place, heightValue, position), Rectangle(widthValue - place, heightValue, newPos));
+}
+
+int Rectangle::width() {
+    return widthValue;
+}
+
+int Rectangle::height() {
+    return heightValue;
+}
+
+Position Rectangle::pos() {
+    return position;
+}
+
+Rectangles::Rectangles() {
+    recsVector = std::vector<Rectangle>();
+}
+
+Rectangles::Rectangles(std::initializer_list<Rectangle> list) {
+    this->recsVector = std::vector<Rectangle>(list);
+}
+
+bool Rectangles::operator ==(const Rectangles& recsObj) const {
+    return (this->recsVector == recsObj.recsVector);
+}
+
+Rectangles& Rectangles::operator +=(Vector& v) {
+    for (int i = 0; i < recsVector.size(); i++) {
+        recsVector[i] += v;
+    }
+    return *this;
+}
+
+Rectangle& Rectangles::operator [](int i) {
+    return recsVector[i];
+}
+
+size_t Rectangles::size() {
+    return recsVector.size();
+}
+
+void Rectangles::split_horizontally(int idx, int place) {
+    std::pair<Rectangle, Rectangle> result = recsVector[idx].split_horizontally(place);
+    recsVector.erase(recsVector.begin() + idx);
+    recsVector.insert(recsVector.begin() + idx, get<0>(result));
+    recsVector.insert(recsVector.begin() + idx + 1, get<1>(result));
+}
+
+void Rectangles::split_vertically(int idx, int place) {
+    std::pair<Rectangle, Rectangle> result = recsVector[idx].split_vertically(place);
+    recsVector.erase(recsVector.begin() + idx);
+    recsVector.insert(recsVector.begin() + idx, get<0>(result));
+    recsVector.insert(recsVector.begin() + idx + 1, get<1>(result));
+}
+
+Position operator +(Position p, Vector& v) {  //IMO jest źle, bo + nie powinien zmieniać żadnego parametru wejściowego
     return p += v;
 }
 
-Position operator + (Vector &v, Position p) {
+Position operator +(Vector& v, Position p) {
     return p += v;
 }
 
-Vector operator + (Vector v1, Vector v2) {
+Vector operator +(Vector v1, Vector v2) {
     return v1 += v2;
 }
 
-Rectangle operator + (Rectangle r, Vector& v) {
+Rectangle operator +(Rectangle r, Vector& v) {
     return r += v;
 }
 
-Rectangle operator + (Vector& v, Rectangle r) {
+Rectangle operator +(Vector& v, Rectangle r) {
     return r += v;
 }
 
-Rectangles operator + (Rectangles r, Vector &v) {
+Rectangles operator +(Rectangles r, Vector& v) {
     return r += v;
 }
 
-Rectangles operator + (Vector &v, Rectangles r) {
+Rectangles operator +(Vector& v, Rectangles r) {
     return r += v;
 }
 
@@ -228,39 +207,30 @@ void merge_vertically(Rectangle rect1, Rectangle rect2) {
     // TODO
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     Vector v1 = Vector(10, 10);
     Position p1 = Position(1, 1);
     Rectangle r1 = Rectangle(100, 100, p1);
     Rectangle r2 = Rectangle(200, 200, p1);
     Rectangle r3 = Rectangle(300, 300, p1);
     Rectangles recs = Rectangles({r1, r2, r3});
-    
-    
+
+
     recs += v1;
-    
+
     recs.split_horizontally(1, 10);
-    
+
     Rectangle r4 = recs[1];
     Rectangle r5 = recs[2];
     Position p4 = r4.pos();
     Position p5 = r5.pos();
-    
+
     cout << r4.width() << " " << r4.height() << endl;
     cout << p4.x() << " " << p4.y() << endl;
     cout << r5.width() << " " << r5.height() << endl;
     cout << p5.x() << " " << p5.y() << endl;
-    
+
     cout << recs.size() << endl;
-    
+
     return 0;
 }
-
-/*
- - funkcja origin w klasie Position
- - funkcja reflection w klasie Rectangle
- - funkcje merge_horizontally i merge_vertically
- - plik naglowkowy .h
- - r-value
- - sprawdzenie poprawnosci argumentow (assert?)
-*/
--- geometry.h
+++ geometry.h
@@ -1,6 +1,122 @@
-#ifndef geometry_h
-#define geometry_h
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+#include <utility>
+
+class Vector {
+private:
+    int firstCord;
+    int secondCord;
+
+public:
+    Vector(int x, int y);
+
+    bool operator ==(const Vector& v) const;
+
+    Vector& operator +=(const Vector& v);
+
+    Vector reflection();
+
+    int x() const;
+
+    int y() const;
+};
+
+class Position {
+private:
+    int firstCord;
+    int secondCord;
+
+public:
+    Position(int x, int y);
+
+    bool operator ==(const Position& p) const;
+
+    Position& operator +=(const Vector& v) {
+        this->firstCord += v.x();
+        this->secondCord += v.y();
+        return *this;
+    }
+
+    Position reflection();
+
+    int x() const;
+
+    int y() const;
+
+    static Position origin();
+};
+
+class Rectangle {
+private:
+    int widthValue;
+    int heightValue;
+    Position position;
+
+public:
+    Rectangle(int width, int height);
+
+    Rectangle(int width, int height, Position pos);
+
+    bool operator ==(const Rectangle& r) const;
+
+    Rectangle& operator +=(Vector& v);
+
+    Rectangle reflection();
+
+    long long area();
+
+    std::pair<Rectangle, Rectangle> split_horizontally(int place);
+
+    std::pair<Rectangle, Rectangle> split_vertically(int place);
+
+    int width();
+
+    int height();
+
+    Position pos();
+};
+
+class Rectangles {
+private:
+    std::vector<Rectangle> recsVector;
+
+public:
+    Rectangles();
+
+    Rectangles(std::initializer_list<Rectangle> list);
+
+    bool operator ==(const Rectangles& recsObj) const;
+
+    Rectangles& operator +=(Vector& v);
+
+    Rectangle& operator [](int i);
+
+    size_t size();
+
+    void split_horizontally(int idx, int place);
+
+    void split_vertically(int idx, int place);
+};
+
+Position operator +(Position p, Vector& v);
+
+Position operator +(Vector& v, Position p);
+
+Vector operator +(Vector v1, Vector v2);
+
+Rectangle operator +(Rectangle r, Vector& v);
+
+Rectangle operator +(Vector& v, Rectangle r);
+
+Rectangles operator +(Rectangles r, Vector& v);
+
+Rectangles operator +(Vector& v, Rectangles r);
+
+void merge_horizontally(Rectangle rect1, Rectangle rect2);
+
+void merge_vertically(Rectangle rect1, Rectangle rect2);
 
 // TODO
 
-#endif /* geometry_h */
+#endif // GEOMETRY_H
